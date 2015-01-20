03849aef1c020df694b69b968754bf68ad038992
nicola_ferraro <nibbio84@gmail.com>
2015-01-20 12:38:42 +0100

    sample integration tests

--- src/test/scala/it/nerdammer/spark/hbase/HBaseReaderBuilderTest.scala
+++ src/test/scala/it/nerdammer/spark/hbase/HBaseReaderBuilderTest.scala
@@ -8,38 +8,7 @@ import org.scalatest.{FlatSpec, Matchers}
  */
 class HBaseReaderBuilderTest extends FlatSpec with Matchers {
 
-  /*"a HBaseRDD" should "allow filtering and counting" in {
 
-    val conf = new SparkConf()
-    conf.set("spark.master", "local")
-    conf.set("spark.driver.allowMultipleContexts", "true")
-    conf.setAppName("test")
-    val sc = new SparkContext(conf)
-
-    val ciccioCount = sc.hbaseTable[(String, String)]("connector")
-      .select("c1", "c2")
-      .inColumnFamily("test1")
-      .filter(_._1=="ciccio")
-      .count
-
-    println("Cicci: " + ciccioCount)
-
-  }*/
-
-  /*"a HBaseRDD" should "allow selecting the row id" in {
-
-    val conf = new SparkConf()
-    conf.set("spark.master", "local")
-    conf.set("spark.driver.allowMultipleContexts", "true")
-    conf.setAppName("test")
-    val sc = new SparkContext(conf)
-
-    val ciccioCount = sc.hbaseTable[(String, String)]("connector")
-      .select("c1")
-      .inColumnFamily("test1")
-      .foreach(println)
-
-  }*/
 
   "ranges" should "be computed correctly" in {
     class TestConversions extends HBaseReaderBuilderConversions
