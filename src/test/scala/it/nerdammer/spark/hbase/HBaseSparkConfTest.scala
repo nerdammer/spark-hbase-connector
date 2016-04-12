5a1314fcda2c99a21ed223ab0e6edc784759cd3c
Nicola Ferraro <nibbio84@gmail.com>
2016-04-13 01:32:31 +0200

    Fixed tests

--- src/test/scala/it/nerdammer/spark/hbase/HBaseSparkConfTest.scala
+++ src/test/scala/it/nerdammer/spark/hbase/HBaseSparkConfTest.scala
@@ -1,20 +1,29 @@
 package it.nerdammer.spark.hbase
 
+import com.user.integration.IntegrationUtils
+import org.apache.hadoop.hbase.HConstants
 import org.apache.spark.SparkConf
 import org.scalatest.{FlatSpec, Matchers}
 
 class HBaseSparkConfTest extends FlatSpec with Matchers {
 
-  "A default HBaseSparkConf" should "have a host property set to localhost" in {
+  "A default HBaseSparkConf" should "have a empty host property" in {
 
     val conf = new HBaseSparkConf
 
-    conf.hbaseHost should be (HBaseSparkConf.DefaultHBaseHost)
+    conf.hbaseHost should be (None)
   }
 
-  "A default HBaseSparkConf object" should "have a host property set to localhost" in {
+  "A default HBaseSparkConf object" should "have a host property set to localhost without other configurations" in {
 
-    val conf = HBaseSparkConf.fromSparkConf(new SparkConf)
-    conf.hbaseHost should be (HBaseSparkConf.DefaultHBaseHost)
+    val hadoopConf = HBaseSparkConf.fromSparkConf(new SparkConf).createHadoopBaseConfig()
+    hadoopConf.get(HConstants.ZOOKEEPER_QUORUM) should be (HBaseSparkConf.DefaultHBaseHost)
+  }
+
+  "the hbase-site.xml file" should "be loaded from the classpath if available" in {
+
+    val hbaseConf = HBaseSparkConf.fromSparkConf(new SparkConf()).createHadoopBaseConfig()
+    assert(hbaseConf.getInt("file.stream-buffer-size", -1) == 8000) // set in test hbase-site.xml file
+
   }
 }
