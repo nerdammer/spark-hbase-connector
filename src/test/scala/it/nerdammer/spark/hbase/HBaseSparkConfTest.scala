9b7701b29b2501410c45f1e25ecdf00eabe33bb4
Nicola Ferraro <nibbio84@gmail.com>
2016-04-13 01:57:14 +0200

    Added tests on precedence

--- src/test/scala/it/nerdammer/spark/hbase/HBaseSparkConfTest.scala
+++ src/test/scala/it/nerdammer/spark/hbase/HBaseSparkConfTest.scala
@@ -16,14 +16,28 @@ class HBaseSparkConfTest extends FlatSpec with Matchers {
 
   "A default HBaseSparkConf object" should "have a host property set to localhost without other configurations" in {
 
-    val hadoopConf = HBaseSparkConf.fromSparkConf(new SparkConf).createHadoopBaseConfig()
-    hadoopConf.get(HConstants.ZOOKEEPER_QUORUM) should be (HBaseSparkConf.DefaultHBaseHost)
+    val hbaseConf = HBaseSparkConf.fromSparkConf(new SparkConf).createHadoopBaseConfig()
+    hbaseConf.get(HConstants.ZOOKEEPER_QUORUM) should be (HBaseSparkConf.DefaultHBaseHost)
   }
 
   "the hbase-site.xml file" should "be loaded from the classpath if available" in {
 
     val hbaseConf = HBaseSparkConf.fromSparkConf(new SparkConf()).createHadoopBaseConfig()
     assert(hbaseConf.getInt("file.stream-buffer-size", -1) == 8000) // set in test hbase-site.xml file
+
+  }
+
+  "the host inside the hbase-site.xml file" should "not be overriden with localhost" in {
+
+    val hbaseConf = new HBaseSparkConf(hbaseXmlConfigFile = "alternative-hbase-site.xml").createHadoopBaseConfig()
+    hbaseConf.get(HConstants.ZOOKEEPER_QUORUM) should be ("anotherhost")
+
+  }
+
+  "the host passed as argument to spark" should "have always precedence" in {
+
+    val hbaseConf = new HBaseSparkConf(hbaseXmlConfigFile = "alternative-hbase-site.xml", hbaseHost = Some("mysparkHost")).createHadoopBaseConfig()
+    hbaseConf.get(HConstants.ZOOKEEPER_QUORUM) should be ("mysparkHost")
 
   }
 }
