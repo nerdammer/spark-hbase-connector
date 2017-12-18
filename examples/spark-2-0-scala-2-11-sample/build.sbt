01db318e7addb19b8c9a26d2d3002197a8e0943b
chetkhatri <ckhatrimanjal@gmail.com>
2017-12-19 00:27:47 +0530

    Examples are added and coding practice improved

--- examples/spark-2-0-scala-2-11-sample/build.sbt
+++ examples/spark-2-0-scala-2-11-sample/build.sbt
@@ -5,15 +5,20 @@ version := "1.0"
 
 scalaVersion := "2.11.0"
 
-libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.0.0" excludeAll(ExclusionRule(organization="joda-time"), ExclusionRule(organization="org.slf4j"), ExclusionRule(organization="com.sun.jersey.jersey-test-framework"), ExclusionRule(organization="org.apache.hadoop"))
+libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.0.1" excludeAll(ExclusionRule(organization="joda-time"), ExclusionRule(organization="org.slf4j"), ExclusionRule(organization="com.sun.jersey.jersey-test-framework"), ExclusionRule(organization="org.apache.hadoop"))
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.0.1" excludeAll(ExclusionRule(organization="joda-time"), ExclusionRule(organization="org.slf4j"), ExclusionRule(organization="com.sun.jersey.jersey-test-framework"), ExclusionRule(organization="org.apache.hadoop"))
 libraryDependencies += "it.nerdammer.bigdata" % "spark-hbase-connector_2.10" % "1.0.3"
 libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.7.0" excludeAll(ExclusionRule(organization="joda-time"), ExclusionRule(organization="org.slf4j"))
 libraryDependencies += "org.apache.commons" % "commons-exec" % "1.3"
+libraryDependencies += "com.typesafe" % "config" % "1.3.0"
+libraryDependencies += "org.slf4j" % "slf4j-api" % "1.7.7"
 
 assemblyMergeStrategy in assembly := {
   case PathList("META-INF", xs @ _*) => MergeStrategy.discard
   case x => MergeStrategy.first
 }
+
+
 
 
     
--- examples/spark-2-0-scala-2-11-sample/src/main/scala/com/chetan/spark/hbasehivepoc/SparkHBaseTransferHiveJob.scala
+++ examples/spark-2-0-scala-2-11-sample/src/main/scala/com/chetan/spark/hbasehivepoc/SparkHBaseTransferHiveJob.scala
@@ -4,37 +4,46 @@ package com.chetan.spark.hbasehivepoc
   * Created by chetan on 28/1/17.
   */
 import it.nerdammer.spark.hbase._
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.types.StringType
 import org.apache.spark.sql.SparkSession
+import com.typesafe.config._
+import org.slf4j.LoggerFactory
 object SparkHBaseTransferHiveJob {
-  val APP_NAME: String = "SparkHbaseJob"
-  var HBASE_DB_HOST: String = null
-  var HBASE_TABLE: String = null
-  var HBASE_COLUMN_FAMILY: String = null
-  var HIVE_DATA_WAREHOUSE: String = null
+  val APP_NAME: String = "spark-hbase-job"
+  val logger = LoggerFactory.getLogger(getClass.getName)
+  var CONFIG_ENV: Config = null
+  var HBASE_DB_HOST: Option[String] = None
+  var HBASE_TABLE: Option[String] = None
+  var HBASE_COLUMN_FAMILY: Option[String] = None
+  var HIVE_DATA_WAREHOUSE: Option[String] = None
   def main(args: Array[String]) = {
+
+    // read configuration from config file
+    CONFIG_ENV = ConfigFactory.load("config.conf")
+
     // Initializing HBASE Configuration variables
-    HBASE_DB_HOST="127.0.0.1"
-    HBASE_TABLE="university"
-    HBASE_COLUMN_FAMILY="emp"
+    HBASE_DB_HOST = Some(CONFIG_ENV.getString(s"${APP_NAME}.hbase-db-host"))
+    HBASE_TABLE = Some(CONFIG_ENV.getString(s"${APP_NAME}.hbase-table"))
+    HBASE_COLUMN_FAMILY = Some(CONFIG_ENV.getString(s"${APP_NAME}.hbase-column-family"))
     // Initializing Hive Metastore configuration
-    HIVE_DATA_WAREHOUSE = "/usr/local/hive/warehouse"
-    // setting spark application
-    val sparkConf = new SparkConf().setAppName(APP_NAME)
-    //initialize the spark context
-    val sparkContext = new SparkContext(sparkConf)
+    HIVE_DATA_WAREHOUSE = Some(CONFIG_ENV.getString(s"${APP_NAME}.hive-data-warehouse"))
+    
+    // Enable Hive with Hive warehouse in SparkSession
+    val spark = SparkSession.builder().appName(APP_NAME).config("spark.sql.warehouse.dir", HIVE_DATA_WAREHOUSE.get).enableHiveSupport().getOrCreate()
+
     //Configuring Hbase host with Spark/Hadoop Configuration
-    sparkContext.hadoopConfiguration.set("spark.hbase.host", HBASE_DB_HOST)
+    spark.sparkContext.hadoopConfiguration.set("spark.hbase.host", HBASE_DB_HOST.get)
+    // using spark implicits implementation
+    import spark.implicits._
+
     // Read HBase Table
-    val hBaseRDD = sparkContext.hbaseTable[(Option[String], Option[String], Option[String], Option[String], Option[String])]("university").select("stid", "name","subject","grade","city").inColumnFamily("emp")
+    val hBaseRDD = spark.sparkContext.hbaseTable[(Option[String], Option[String], Option[String], Option[String], Option[String])]("university").select("stid", "name","subject","grade","city").inColumnFamily("emp")
     // Iterate HBaseRDD and generate RDD[Row]
     val rowRDD = hBaseRDD.map(i => Row(i._1.get,i._2.get,i._3.get,i._4.get,i._5.get))
-    // Create sqlContext for createDataFrame method
-    val sqlContext = new org.apache.spark.sql.SQLContext(sparkContext)
+    
     // Create Schema Structure
     object empSchema {
       val stid = StructField("stid", StringType)
@@ -45,15 +54,9 @@ object SparkHBaseTransferHiveJob {
       val struct = StructType(Array(stid, name, subject, grade, city))
     }
 
-    import sqlContext.implicits._
     // Create DataFrame with rowRDD and Schema structure
-    val stdDf = sqlContext.createDataFrame(rowRDD,empSchema.struct);
-    // Enable Hive with Hive warehouse in SparkSession
-    val spark = SparkSession.builder().appName(APP_NAME).config("spark.sql.warehouse.dir", HIVE_DATA_WAREHOUSE).enableHiveSupport().getOrCreate()
-    // Importing spark implicits and sql package
-    import spark.implicits._
-    import spark.sql
-
+    val stdDf = spark.sqlContext.createDataFrame(rowRDD,empSchema.struct)
+    
     // Saving Dataframe to Hive Table Successfully.
     stdDf.write.mode("append").saveAsTable("employee")
 
